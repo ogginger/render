2347c2132fd343bfddd4a7d32e194bc1e4223444
Josh <og.ginger@gmail.com>
2020-08-01 22:40:50 +0000

    Updated build.js and render.min.js: Updated the minified file to reflect the recent changes.

--- build.js
+++ build.js
@@ -9,9 +9,10 @@
 	"file": "lib/file.min",
 	"rsvp": "lib/rsvp.min",
 	"promise": "lib/promise.min",
-	"fs": "empty:" //node module
+	"fs": "empty:", //node module
+	"validate": "lib/validate.min"
     },
-    exclude: [ "underscore", "file", "rsvp", "fs" ],
+    exclude: [ "underscore", "file", "rsvp", "fs", "validate" ],
     name: "render",
     out: "render.min.js"
 })
